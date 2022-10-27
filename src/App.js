6b7985a46fa83c84b269152f88782642b57d00a0
Ursa-Nadeem <101786151+Ursa-Nadeem@users.noreply.github.com>
2022-10-27 17:22:56 +0500

    changes in bottom header

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     <>
      <Headertop/>
 <Headerbottom />
-     <Sidebar/>
+     {/* <Sidebar/> */}
 
     </>
   )
--- src/Component/Bottomheader/index.jsx
+++ src/Component/Bottomheader/index.jsx
@@ -33,6 +33,7 @@ const Headerbottom = () => {
                 </div>
             </div>
            
+           
         </>
     )
 }
