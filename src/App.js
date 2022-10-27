388082441514ae1b4f268b65ab8da6819710d95a
Ursa-Nadeem <101786151+Ursa-Nadeem@users.noreply.github.com>
2022-10-27 15:32:07 +0500

    feat/ bottom header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./App.css"
+import Headerbottom from './Component/Bottomheader'
 import Headertop from './Component/Headertop'
 
 const App = () => {
   return (
     <>
      <Headertop/>
-
+<Headerbottom />
     </>
   )
 }
